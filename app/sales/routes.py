754496ba7be6a4724e8ad74bdf652ff56a213aef
ARAUJO José <jose.araujo-firmino-de-lima@etu.univ-grenoble-alpes.fr>
2025-04-23 16:44:22 +0200

    Criação das rotas API GET /api/v1/sales/user/<user_id>, /api/v1/sales e /api/v1/sale_items/<sale_id>

--- app/sales/routes.py
+++ app/sales/routes.py
@@ -6,29 +6,73 @@ Création: jojo, le 12/04/2025
 import uuid
 
 import flask
+from flask import session, jsonify
+
 from app.sales.models import Sale, SaleItem
+from app.auth.routes import get_user_name_by_id
+from utils.auth import token_required
 
 sales_bp = flask.Blueprint('sales_bp', __name__)
+sale_items_bp = flask.Blueprint('sale_items_bp', __name__)
+
+@sale_items_bp.route('/<sale_id>')
+@token_required
+def sale_items(current_user, sale_id):
+    sale_items = SaleItem.query.filter_by(sale_id=sale_id).all()
+    if len(sale_items) == 0:
+        return jsonify({"message": f"The sale {sale_id} has no items."}), 201
+    return jsonify([{"item_id": s.item_id, "quantity": s.quantity, "subtotal": s.subtotal, "user_id": s.user_id,
+                     "sale_id": s.sale_id, "product_id": s.product_id} for s in sale_items])
+
 
 @sales_bp.route('/', methods=('GET', 'POST'))
-def sales():
+@token_required
+def sales(current_user):
     if flask.request.method == 'POST':
         pass
+    if current_user.role == 'admin':
+        sales = Sale.query.all()
+        return get_sales_json(sales)
+    else:
+        session_user = session.get("user_id")
+        if current_user.user_id == session_user:
+            sales = Sale.query.filter_by(user_id=current_user.user_id).all()
+            return get_sales_json(sales)
+        return jsonify({'message': 'You need to login before.'}), 405
 
-    sales = Sale.query.all()
+@sales_bp.route('/user/<user_id>')
+@token_required
+def sales_user(current_user, user_id):
+    sales = []
+    if current_user.role == "admin":
+        sales = Sale.query.filter_by(user_id=uuid.UUID(user_id)).all()
+        if len(sales) == 0:
+            return jsonify({"message": f"This user {get_user_name_by_id(user_id)} has no sales yet."}), 201
+    else:
+        sales = Sale.query.filter_by(user_id=current_user.user_id)
+        if sales is None:
+            return jsonify({"message": f"This user {current_user.name} has no sales yet."}), 201
+
+    return [{"sale_id": s.sale_id,
+                 "sale_datetime": s.sale_datetime,
+                 "total": s.total,
+                 "payment_method": s.payment_method,
+                 "user_id": s.user_id} for s in sales]
+
+@sales_bp.route('/itens/<sale_id>')
+def itens_sale(sale_id):
+    itens = SaleItem.query.filter_by(item_id=uuid.UUID(sale_id))
+
+    return get_itens_json(itens)
+
+def get_itens_json(itens):
     return flask.jsonify(
-        [{"sale_id": s.sale_id, "sale_datetime": s.sale_datetime, "total": s.total, "payment_method": s.payment_method, "user_id": s.user_id}
+        [{"item_id": si.item_id, "quantity": si.quantity, "subtotal": si.subtotal, "user_id": si.user_id,
+          "sale_id": si.sale_id, "product_id": si.product_id}
+         for si in itens])
+
+def get_sales_json(sales):
+    return flask.jsonify(
+        [{"sale_id": s.sale_id, "sale_datetime": s.sale_datetime, "total": s.total, "payment_method": s.payment_method,
+          "user_id": s.user_id}
          for s in sales])
-
-@sales_bp.route('/<user_id>')
-def sales_user(user_id):
-    sales = SaleItem.query.filter_by(user_id=uuid.UUID(user_id))
-
-    return flask.jsonify(
-        [{"id": item.id,
-          "quantity": item.quantity,
-          "subtotal": item.subtotal,
-          "user_id": item.user_id,
-          "sale_id": item.sale_id,
-          "product_id": item.product_id
-          } for item in sales])
--- init_db.py
+++ init_db.py
@@ -5,20 +5,14 @@ Création: jojo, le 11/04/2025
 """
 # Imports
 import logging
-import uuid
-
-from werkzeug.routing import UUIDConverter
 
 from app import create_app
-from app.extensions import db
 
 # Configurations globales
 logging.getLogger().setLevel(logging.DEBUG)
 
-
 from app.products.models import Product
 from app.sales.models import Sale, SaleItem
-from app.auth.models import User
 from app import db
 from app.auth.models import User
 import uuid
@@ -28,12 +22,15 @@ import uuid
 def main():
     app = create_app()
     with app.app_context():
+        db.drop_all()
         db.create_all()
         print("Tabelas criadas com sucesso!")
 
-        db.session.query(Product).delete()
-        db.session.commit()
-
+        # db.session.execute(db.text("DELETE FROM sale_items"))
+        # db.session.execute(db.text("DELETE FROM sales"))
+        # db.session.execute(db.text("DELETE FROM users"))
+        # db.session.query(Product).delete()
+        # db.session.commit()
 
         p1 = Product(name="Coxinha", unit_price=5.0, stock=10)
         p2 = Product(name="Pão Francês", unit_price=1.0, stock=100)
@@ -43,11 +40,6 @@ def main():
         db.session.commit()
 
         print("Itens adicionados!")
-
-        db.session.execute(db.text("DELETE FROM sale_items"))
-        db.session.execute(db.text("DELETE FROM sales"))
-        db.session.execute(db.text("DELETE FROM users"))
-        db.session.commit()
 
         u1 = User(
             user_id=uuid.UUID("768e0770-37d7-4485-8ef0-724a593db3d3"),
@@ -63,35 +55,147 @@ def main():
             password="87654321",
             role="user")
 
-        db.session.add_all([u1, u2])
+        u3 = User(
+            user_id=uuid.UUID("bdf9b718-1f36-435a-b39c-895c72983e55"),
+            name="Toto",
+            email="toto@gmail.com",
+            password="11111111",
+            role="user")
+
+        db.session.add_all([u1, u2, u3])
         db.session.commit()
 
         print("Usuarios adicionados!")
 
-        db.session.query(Sale).delete()
-        user_id = uuid.UUID("bdf9b718-1f36-435a-b39c-895c72987e32")
+        print("User Maria vendas:")
+        user_id = u2.user_id
         user = User.query.filter_by(user_id=user_id).first()
         if user:
             sale_id = uuid.uuid4()
+            s1 = Sale(sale_id=sale_id, total=0.0, user_id=user.user_id)
 
-            s1 = Sale(sale_id=sale_id, total=35.0, payment_method="pix", user_id=user.user_id)
-            db.session.add_all([s1])
+            quantity = 5
+            i1 = SaleItem(
+                quantity=quantity,
+                product_id=p1.product_id,
+                subtotal=(p1.unit_price * quantity),
+                user_id=user_id,
+                sale_id=sale_id)
+            s1.total += float(i1.subtotal)
+
+            quantity = 2
+            i2 = SaleItem(
+                quantity=quantity,
+                subtotal=(p3.unit_price * quantity),
+                user_id=user_id,
+                sale_id=sale_id,
+                product_id=p3.product_id)
+
+            s1.total += float(i2.subtotal)
+            payment_method = "pix"
+            s1.payment_method = payment_method
+            db.session.add(s1)
             db.session.commit()
-            print("Venda adicionada!")
-
-
-
-            i1 = SaleItem(quantity=5, subtotal=5.0, user_id=user_id, sale_id=sale_id, product_id=p1.product_id)
-            i2 = SaleItem(quantity=6, subtotal=6.0, user_id=user_id, sale_id=sale_id, product_id=p2.product_id)
-
+            print("Venda 1 adicionada!")
             db.session.add_all([i1, i2])
             db.session.commit()
-            print("itens vendidos adicionados!")
+            print("Itens da venda 1 adicionados!")
 
+
+            sale_id2 = uuid.uuid4()
+            s2 = Sale(sale_id=sale_id2, total=0.0, user_id=user.user_id)
+
+            quantity = 20
+            i3 = SaleItem(
+                quantity=quantity,
+                product_id=p2.product_id,
+                subtotal=(p2.unit_price * quantity),
+                user_id=user_id,
+                sale_id=sale_id2)
+
+            s2.total += float(i3.subtotal)
+
+            quantity = 2
+            i4 = SaleItem(
+                quantity=quantity,
+                subtotal=(p3.unit_price * quantity),
+                user_id=user_id,
+                sale_id=sale_id2,
+                product_id=p3.product_id)
+
+            s2.total += float(i4.subtotal)
+            payment_method = "Debito"
+            s2.payment_method = payment_method
+            db.session.add(s2)
+            db.session.commit()
+            print("Venda 2 adicionada!")
+            db.session.add_all([i1, i2])
+            db.session.commit()
+            print("Itens da venda 2 adicionados!")
 
         else:
             print("User invalido!")
 
+        print("User Pedro vendas:")
+        user_id = u3.user_id
+        user = User.query.filter_by(user_id=user_id).first()
+
+        if user:
+            sale_id = uuid.uuid4()
+            s1 = Sale(sale_id=sale_id, total=0.0, user_id=user.user_id)
+
+            quantity = 2
+            i1 = SaleItem(
+                quantity=quantity,
+                product_id=p2.product_id,
+                subtotal=(p2.unit_price * quantity),
+                user_id=user_id,
+                sale_id=sale_id)
+            s1.total += float(i1.subtotal)
+
+            quantity = 1
+            i2 = SaleItem(
+                quantity=quantity,
+                subtotal=(p3.unit_price * quantity),
+                user_id=user_id,
+                sale_id=sale_id,
+                product_id=p3.product_id)
+
+            s1.total += float(i2.subtotal)
+            payment_method = "Dinheiro"
+            s1.payment_method = payment_method
+            db.session.add(s1)
+            db.session.commit()
+
+            print("Venda 1 adicionada!")
+            db.session.add_all([i1, i2])
+            db.session.commit()
+            print("Itens da venda 1 adicionados!")
+
+
+            sale_id2 = uuid.uuid4()
+            s2 = Sale(sale_id=sale_id2, total=0.0, user_id=user.user_id)
+
+            quantity = 20
+            i3 = SaleItem(
+                quantity=quantity,
+                product_id=p2.product_id,
+                subtotal=(p2.unit_price * quantity),
+                user_id=user_id,
+                sale_id=sale_id2)
+
+            s2.total += float(i3.subtotal)
+            payment_method = "Pix"
+            s2.payment_method = payment_method
+            db.session.add(s2)
+            db.session.commit()
+            print("Venda 2 adicionada!")
+            db.session.add(i1)
+            db.session.commit()
+            print("Itens da venda 2 adicionados!")
+
+        else:
+            print("User invalido!")
 
 
 if __name__ == '__main__':
