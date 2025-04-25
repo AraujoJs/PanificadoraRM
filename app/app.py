8de9da8190a2856023d67f59587611e774fe8b34
ARAUJO José <jose.araujo-firmino-de-lima@etu.univ-grenoble-alpes.fr>
2025-04-25 23:37:29 +0200

    redirecionamento root

--- app/app.py
+++ app/app.py
@@ -1,9 +1,16 @@
+from flask import url_for, redirect
+
 from app import create_app
 
 
 # Cria e roda a aplicação Flask
 app = create_app()
 app.config['SESSION_PERMANENT'] = False
+
+@app.route('/')
+def index():
+    return redirect(url_for('auth_bp.login'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
