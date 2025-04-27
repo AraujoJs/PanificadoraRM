e5135822cfd0f1b27c74466598ab6a3ae04cfe45
ARAUJO José <jose.araujo-firmino-de-lima@etu.univ-grenoble-alpes.fr>
2025-04-27 13:19:24 +0200

    refatoração do codigo

--- app/products/routes.py
+++ app/products/routes.py
@@ -3,53 +3,57 @@
 Script: Backend/rotues
 Création: jojo, le 12/04/2025
 """
-from crypt import methods
-from tkinter.font import names
 
 from flask import Blueprint, jsonify, request
-from flask.globals import request_ctx
+
 from app.extensions import db
 from app.products.models import Product
 from utils.auth import token_required
+
 # Imports
 
 
 # Configurations globales
 produtos = Blueprint('products_bp', __name__)
 
-@produtos.route('/', methods=['GET', 'POST'])
+
+@produtos.route('/', methods=['GET'])
 @token_required
 def products(current_user):
-    if request.method == 'POST':
-        if current_user.role != 'admin':
-            return jsonify({'message': 'Acesso negado'}), 403
+    all_products = Product.query.all()
+    products_list = [
+        {
+            "product_id": p.product_id,
+            "name": p.name,
+            "unit_price": p.unit_price,
+            "stock": p.stock
+        } for p in all_products
+    ]
+    return jsonify(products_list)
 
-        data = request.get_json() or {}
-        name = data.get('name')
-        unit_price = data.get('unit_price')
-        stock = data.get('stock')
 
-        if not all([name, unit_price, stock]):
-            return jsonify({'message': 'Nome, preço unitário e estoque são obrigatórios.'}), 400
+@produtos.route('/', methods=['POST'])
+@token_required
+def add_product(current_user):
+    if current_user.role != 'admin':
+        return jsonify({'message': 'Acesso negado'}), 403
 
-        try:
-            new_product = Product(name=name, unit_price=unit_price, stock=stock)
-            db.session.add(new_product)
-            db.session.commit()
-            return jsonify({'message': f'Produto {new_product.name} criado com sucesso!'}), 201
+    data = request.get_json() or {}
+    name = data.get('name')
+    unit_price = data.get('unit_price')
+    stock = data.get('stock')
 
-        except Exception as e:
-            db.session.rollback()
-            return jsonify({'message': f'Ocorreu um erro: {str(e)}'}), 500
+    if not all([name, unit_price, stock]):
+        return jsonify({'message': 'Nome, preço unitário e estoque são obrigatórios.'}), 400
 
-    elif request.method == 'GET':
-        all_products = Product.query.all()
-        products_list = [
-            {
-                "product_id": p.product_id,
-                "name": p.name,
-                "unit_price": p.unit_price,
-                "stock": p.stock
-            } for p in all_products
-        ]
-        return jsonify(products_list)
+    try:
+        new_product = Product(name=name, unit_price=unit_price, stock=stock)
+        db.session.add(new_product)
+        db.session.commit()
+        return jsonify({'message': f'Produto {new_product.name} criado com sucesso!'}), 201
+
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'message': f'Ocorreu um erro: {str(e)}'}), 500
+
+
