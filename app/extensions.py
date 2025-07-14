0d1098abbf9e7480e66fa9a9d7ee5bee8929f567
ARAUJO José <jose.araujo-firmino-de-lima@etu.univ-grenoble-alpes.fr>
2025-07-14 13:54:39 -0300

    solving bug env variable secret key

--- app/extensions.py
+++ app/extensions.py
@@ -7,7 +7,11 @@ Création: jojo, le 12/04/2025
 
 # Imports
 import os
+
+from flask.cli import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
 
 db = SQLAlchemy()
 SECRET_KEY = os.getenv('SECRET_KEY')
